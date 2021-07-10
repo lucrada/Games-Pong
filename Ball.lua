13d31ed2081b631929937af30c93aa58edab9fa4
Dracula <38753678+lucrada@users.noreply.github.com>
2021-07-10 19:24:35 +0530

    Update Ball.lua

--- Ball.lua
+++ Ball.lua
@@ -6,7 +6,6 @@ function Ball:init(x, y, width, height)
     self.width = width
     self.height = height
 
-  --self.dx = math.random(2) == 1 and 100 or -100
     self.dx = -100
     self.dy = math.random(-50, 50) * 1.5
 end
@@ -15,7 +14,6 @@ function Ball:reset()
     self.x = VIRTUAL_WIDTH/2 - 2
     self.y = VIRTUAL_HEIGHT/2 - 2
 
-    --self.dx = math.random(2) == 1 and 100 or -100
     self.dx = -100
     self.dy = math.random(-50, 50) * 1.5
 
